6cf8e5cdc510ed1f6f7def845f81278466ba5b57
pjho4746 <pjho4746@gmail.com>
2023-07-09 16:24:47 +0900

    Member / Security

---
+++ src/main/java/com/example/hanul/HanulApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hanul/HanulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
