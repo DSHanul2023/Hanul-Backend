aef7a67cf360e5693384758dbd41eda4d61eefcb
jiho-par <hkp314518@duksung.ac.kr>
2023-08-02 14:04:29 +0900

    페이지네이션을 이용하여 TMDB API에서 모든 영화 데이터 가져오도록 수정 (TMDB 데이터 제한 -> 최대 페이지 수 500)

--- src/main/java/com/example/hanul/DataInitializer.java
+++ src/main/java/com/example/hanul/DataInitializer.java
@@ -2,10 +2,10 @@ package com.example.hanul;
 
 import com.example.hanul.dto.ItemDTO;
 import com.example.hanul.dto.TMDBMovieDTO;
-
 import com.example.hanul.model.ItemEntity;
 import com.example.hanul.response.TMDBMovieListResponse;
 import com.example.hanul.service.ItemService;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,9 +16,11 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-@Slf4j
 @Component
+@Slf4j
 public class DataInitializer implements CommandLineRunner {
+    @JsonProperty("total_pages")
+    private int totalPages;
 
     private final ItemService itemService;
     private final WebClient webClient;
@@ -38,40 +40,60 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void registerTMDBMovies() {
-        String apiKey = itemService.getTmdbApiKey();
+        String apiKey = getTmdbApiKey();
+        int currentPage = 1;
 
-        // TMDB API에서 영화 목록 가져오기
-        String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
-        Mono<TMDBMovieListResponse> responseMono = webClient.get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(TMDBMovieListResponse.class);
+        while (currentPage <= 500) { // 최대 500 페이지까지 생성하도록 수정
+            // TMDB API에서 영화 목록 가져오기 (페이지별로 데이터 요청)
+            String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&page=" + currentPage;
+            Mono<TMDBMovieListResponse> responseMono = webClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(TMDBMovieListResponse.class);
 
-        // 응답 데이터에서 필요한 정보 추출하여 ItemEntity에 등록
-        TMDBMovieListResponse movieListResponse = responseMono.block();
-        if (movieListResponse != null) {
-            List<TMDBMovieDTO> movies = movieListResponse.getResults();
-            for (TMDBMovieDTO movie : movies) {
-                ItemDTO itemDTO = new ItemDTO();
-                itemDTO.setItemNm(movie.getTitle());
-                itemDTO.setItemDetail(movie.getOverview());
+            try {
+                // 응답 데이터에서 필요한 정보 추출하여 ItemEntity에 등록
+                TMDBMovieListResponse movieListResponse = responseMono.block();
+                if (movieListResponse != null) {
+                    List<TMDBMovieDTO> movies = movieListResponse.getResults();
+                    for (TMDBMovieDTO movie : movies) {
+                        ItemDTO itemDTO = new ItemDTO();
+                        itemDTO.setItemNm(movie.getTitle());
+                        itemDTO.setItemDetail(movie.getOverview());
 
-                // 포스터 URL을 기본 URL과 poster_path를 이용하여 구성
-                String posterUrl = "https://image.tmdb.org/t/p/w500" + movie.getPosterPath();
-                itemDTO.setPosterUrl(posterUrl);
+                        // 포스터 URL을 기본 URL과 poster_path를 이용하여 구성
+                        String posterUrl = "https://image.tmdb.org/t/p/w500" + movie.getPosterPath();
+                        itemDTO.setPosterUrl(posterUrl);
 
-                // ItemEntity를 생성하고 포스터 URL을 설정한 후 아이템으로 등록합니다.
-                ItemEntity itemEntity = ItemEntity.builder()
-                        .itemNm(itemDTO.getItemNm())
-                        .itemDetail(itemDTO.getItemDetail())
-                        .posterUrl(itemDTO.getPosterUrl()) // 포스터 URL 설정
-                        .build();
+                        // ItemEntity를 생성하고 포스터 URL을 설정한 후 아이템으로 등록합니다.
+                        ItemEntity itemEntity = ItemEntity.builder()
+                                .itemNm(itemDTO.getItemNm())
+                                .itemDetail(itemDTO.getItemDetail())
+                                .posterUrl(itemDTO.getPosterUrl()) // 포스터 URL 설정
+                                .build();
 
-                itemService.saveItemWithPoster(itemEntity);
+                        itemService.saveItemWithPoster(itemEntity);
+                    }
+                    log.info("페이지 " + currentPage + "의 TMDB 데이터 등록이 완료되었습니다.");
+
+                    // 총 페이지 수 갱신
+                    totalPages = movieListResponse.getTotalPages();
+                    currentPage++;
+                } else {
+                    log.error("TMDB 데이터 등록 중 오류가 발생하였습니다.");
+                    break;
+                }
+            } catch (Exception e) {
+                log.error("TMDB 데이터 등록 중 오류가 발생하였습니다.", e);
+                break;
             }
-            log.info("TMDB 데이터 등록이 완료되었습니다.");
-        } else {
-            log.error("TMDB 데이터 등록 중 오류가 발생하였습니다.");
         }
+
+        log.info("모든 TMDB 데이터 등록이 완료되었습니다.");
+        log.info("총 등록된 아이템 수: " + itemService.getTotalItemCount());
+    }
+
+    private String getTmdbApiKey() {
+        return tmdbApiKey;
     }
 }
--- src/main/java/com/example/hanul/response/TMDBMovieListResponse.java
+++ src/main/java/com/example/hanul/response/TMDBMovieListResponse.java
@@ -1,21 +1,26 @@
 package com.example.hanul.response;
 
 import com.example.hanul.dto.TMDBMovieDTO;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class TMDBMovieListResponse {
     @JsonProperty("results")
     private List<TMDBMovieDTO> results;
 
-    public List<TMDBMovieDTO> getResults() {
-        return results;
+    @JsonProperty("total_pages")
+    private int totalPages;
+
+    // Getter and Setter for totalPages
+
+    public int getTotalPages() {
+        return totalPages;
     }
 
-    public void setResults(List<TMDBMovieDTO> results) {
-        this.results = results;
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
     }
 }
