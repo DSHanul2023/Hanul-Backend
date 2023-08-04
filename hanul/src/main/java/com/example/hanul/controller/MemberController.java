e13c0e07093096de2d8eeba9bce2bddaeb5429fc
chaeeunnn <jytp922@gmail.com>
2023-08-04 12:25:11 +0900

    getMemberInfo, updateMemberInfo 추가

--- hanul/src/main/java/com/example/hanul/controller/MemberController.java
+++ hanul/src/main/java/com/example/hanul/controller/MemberController.java
@@ -6,7 +6,10 @@ import com.example.hanul.service.MemberService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/members")
@@ -27,14 +30,24 @@ public class MemberController {
         }
     }
 
+//    @PostMapping("/login")
+//    public ResponseEntity<Object> login(@RequestBody MemberDTO memberDTO, @Value("${jwt.secret}") String secret) {
+//        String token = memberService.loginAndGetToken(memberDTO.getEmail(), memberDTO.getPassword(), secret);
+//        if (token != null) {
+//            String memberId = memberService.extractIdFromToken(token,secret); // 토큰에서 memberId 추출
+//            memberDTO.setId(memberId); // InquiryDTO에 memberId 설정
+//
+//            return ResponseEntity.ok("{\"message\": \"Login successful\", \"token\": \"" + token + "\"}");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"message\": \"Invalid credentials\"}");
+//        }
+//    }
+
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody MemberDTO memberDTO, @Value("${jwt.secret}") String secret) {
-        String token = memberService.loginAndGetToken(memberDTO.getEmail(), memberDTO.getPassword(), secret);
-        if (token != null) {
-            String memberId = memberService.extractIdFromToken(token,secret); // 토큰에서 memberId 추출
-            memberDTO.setId(memberId); // InquiryDTO에 memberId 설정
-
-            return ResponseEntity.ok("{\"message\": \"Login successful\", \"token\": \"" + token + "\"}");
+        MemberDTO loggedInMember = memberService.loginAndGetMemberInfo(memberDTO.getEmail(), memberDTO.getPassword(), secret);
+        if (loggedInMember != null) {
+            return ResponseEntity.ok(loggedInMember);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"message\": \"Invalid credentials\"}");
         }
@@ -45,4 +58,41 @@ public class MemberController {
         memberService.logout();
         return ResponseEntity.ok("{\"message\": \"Logout successful\"}");
     }
+
+    @GetMapping("/getMemberInfo")
+    public ResponseEntity<MemberDTO> getMemberInfo(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            token = token.substring(7); // Remove "Bearer " prefix
+            MemberDTO memberDTO = memberService.getMemberInfo(token);
+
+            if (memberDTO != null) {
+                return ResponseEntity.ok(memberDTO);
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
+
+    @PostMapping("/updateMemberInfo")
+    public ResponseEntity<MemberDTO> updateMemberInfo(@RequestBody MemberDTO memberDTO) {
+        boolean success = memberService.updateMemberInfo(memberDTO);
+        if (success) {
+            MemberDTO updatedMember = memberService.getMemberInfo(memberDTO.getToken());
+            if (updatedMember != null) {
+                return ResponseEntity.ok(updatedMember);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(null);
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+    }
+
+
+
+
 }
