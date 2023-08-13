c54e96b547b17a09e59e5af8225661e866131055
pjho4746 <pjho4746@gmail.com>
2023-08-13 14:31:10 +0900

    @PostMapping("/chatdialogflow") 수정

--- src/main/java/com/example/hanul/controller/ChatController.java
+++ src/main/java/com/example/hanul/controller/ChatController.java
@@ -6,18 +6,24 @@ import com.example.hanul.model.ChatEntity;
 import com.example.hanul.service.ChatService;
 
 import com.example.hanul.service.DialogflowService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/chats")
 public class ChatController {
     private final ChatService chatService;
     private final DialogflowService dialogflowService; // Dialogflow 서비스 추가
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class); // Logger 추가
+
 
     @Autowired
     public ChatController(ChatService chatService, DialogflowService dialogflowService) {
@@ -40,13 +46,12 @@ public class ChatController {
                 String userMessage = chatDTO.getMessage();
                 String dialogflowResponse = dialogflowService.sendToDialogflow(userMessage);
 
-                String responseMessage = "[사용자 채팅 저장 및 응답]\n"
-                        + "입력: " + userMessage + "\n"
-                        + "응답: " + dialogflowResponse;
+                // 로그로 출력
+                logger.info("[사용자 채팅 저장 및 응답] 입력: {} 응답: {}", userMessage, dialogflowResponse);
 
-                return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+                return ResponseEntity.status(HttpStatus.OK).body(dialogflowResponse); // 응답은 dialogflowResponse로만 설정
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("Dialogflow 통신 실패", e);
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to communicate with Dialogflow");
             }
         } else {
