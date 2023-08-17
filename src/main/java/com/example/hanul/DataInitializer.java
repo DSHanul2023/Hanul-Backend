4d6e6c247251dcd7c59234178ace778c263e2186
pjho4746 <pjho4746@gmail.com>
2023-08-17 15:44:22 +0900

    TMDB 장르 1차

--- src/main/java/com/example/hanul/DataInitializer.java
+++ src/main/java/com/example/hanul/DataInitializer.java
@@ -1,11 +1,12 @@
 package com.example.hanul;
 
+import com.example.hanul.dto.GenreDTO;
 import com.example.hanul.dto.ItemDTO;
 import com.example.hanul.dto.TMDBMovieDTO;
 import com.example.hanul.model.ItemEntity;
+import com.example.hanul.repository.ItemRepository;
 import com.example.hanul.response.TMDBMovieListResponse;
-import com.example.hanul.service.ItemService;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,26 +15,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
-    @JsonProperty("total_pages")
-    private int totalPages;
 
-    public int getTotalPages() {
-        return totalPages;
-    }
-    private final ItemService itemService;
+    private final ItemRepository itemRepository;
     private final WebClient webClient;
 
     @Value("${tmdb.api.key}")
     private String tmdbApiKey;
 
     @Autowired
-    public DataInitializer(ItemService itemService, WebClient.Builder webClientBuilder) {
-        this.itemService = itemService;
+    public DataInitializer(ItemRepository itemRepository, WebClient.Builder webClientBuilder) {
+        this.itemRepository = itemRepository;
         this.webClient = webClientBuilder.build();
     }
 
@@ -46,8 +44,7 @@ public class DataInitializer implements CommandLineRunner {
         String apiKey = getTmdbApiKey();
         int currentPage = 1;
 
-        while (currentPage <= 500) { // 최대 500 페이지까지 생성하도록 수정
-            // TMDB API에서 한국어 영화 목록 가져오기 (페이지별로 데이터 요청, language 파라미터 추가)
+        while (currentPage <= 500) { // 최대 500 페이지까지 데이터를 가져옴
             String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&page=" + currentPage + "&language=ko-KR";
             Mono<TMDBMovieListResponse> responseMono = webClient.get()
                     .uri(url)
@@ -55,32 +52,44 @@ public class DataInitializer implements CommandLineRunner {
                     .bodyToMono(TMDBMovieListResponse.class);
 
             try {
-                // 응답 데이터에서 필요한 정보 추출하여 ItemEntity에 등록
                 TMDBMovieListResponse movieListResponse = responseMono.block();
                 if (movieListResponse != null) {
                     List<TMDBMovieDTO> movies = movieListResponse.getResults();
+
+                    // 중복을 확인하고 영화를 처리하여 저장
                     for (TMDBMovieDTO movie : movies) {
+                        String movieTitle = movie.getTitle();
+                        if (itemRepository.findByItemNm(movieTitle) != null) {
+                            log.info("이미 등록된 영화: " + movieTitle);
+                            continue;
+                        }
+
                         ItemDTO itemDTO = new ItemDTO();
-                        itemDTO.setItemNm(movie.getTitle());
+                        itemDTO.setItemNm(movieTitle);
                         itemDTO.setItemDetail(movie.getOverview());
 
-                        // 포스터 URL을 기본 URL과 poster_path를 이용하여 구성
+                        // 장르가 있을 경우 설정
+                        List<GenreDTO> genres = movie.getGenres();
+                        if (genres != null && !genres.isEmpty()) {
+                            String genreString = genres.stream()
+                                    .map(GenreDTO::getName)
+                                    .collect(Collectors.joining(", "));
+                            itemDTO.setItemGenre(genreString);
+                        }
+
                         String posterUrl = "https://image.tmdb.org/t/p/w500" + movie.getPosterPath();
                         itemDTO.setPosterUrl(posterUrl);
 
-                        // ItemEntity를 생성하고 포스터 URL을 설정한 후 아이템으로 등록합니다.
                         ItemEntity itemEntity = ItemEntity.builder()
                                 .itemNm(itemDTO.getItemNm())
                                 .itemDetail(itemDTO.getItemDetail())
-                                .posterUrl(itemDTO.getPosterUrl()) // 포스터 URL 설정
+                                .itemGenre(itemDTO.getItemGenre())
+                                .posterUrl(itemDTO.getPosterUrl())
                                 .build();
 
-                        itemService.saveItemWithPoster(itemEntity);
+                        itemRepository.save(itemEntity);
                     }
-                    log.info("페이지 " + currentPage + "의 TMDB 데이터 등록이 완료되었습니다.");
 
-                    // 총 페이지 수 갱신
-                    totalPages = movieListResponse.getTotalPages();
                     currentPage++;
                 } else {
                     log.error("TMDB 데이터 등록 중 오류가 발생하였습니다.");
@@ -93,7 +102,7 @@ public class DataInitializer implements CommandLineRunner {
         }
 
         log.info("모든 TMDB 데이터 등록이 완료되었습니다.");
-        log.info("총 등록된 아이템 수: " + itemService.getTotalItemCount());
+        log.info("총 등록된 아이템 수: " + itemRepository.count());
     }
 
     private String getTmdbApiKey() {
