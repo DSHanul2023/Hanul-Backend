51ea0f8ac1f582aa59f5e3d358356583abc4a0bc
jiho-par <hkp314518@duksung.ac.kr>
2023-08-28 14:35:21 +0900

    Survey JSON 파싱 오류 해결

--- src/main/java/com/example/hanul/controller/SurveyController.java
+++ src/main/java/com/example/hanul/controller/SurveyController.java
@@ -3,40 +3,45 @@ package com.example.hanul.controller;
 import com.example.hanul.dto.BoardDTO;
 import com.example.hanul.dto.ResponseDTO;
 import com.example.hanul.dto.SurveyDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/survey")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SurveyController {
     @PostMapping
-    public ResponseEntity<String> processSurvey(@RequestBody SurveyDTO surveyDTO) {
+    public ResponseEntity<ResponseDTO<String>> processSurvey(@RequestBody SurveyDTO surveyDTO) {
         // 클라이언트로부터 받은 정보를 이용하여 추천 로직 수행
         String category = surveyDTO.getCategory();
         List<String> selectedItems = surveyDTO.getSelectedItems();
 
-        // 여기에 추천 로직을 구현하고 결과를 생성하여 반환하면 됩니다.
-        // 예시로 응답을 만들어 반환하겠습니다.
+        // 여기에 추천 로직을 구현하고 결과를 생성하여 ResponseDTO에 담기
         String response = "선택한 " + category + " : " + selectedItems.toString() + " 에 대한 추천 결과입니다.";
 
-        return ResponseEntity.ok(response);
-    }
-    public String handleFlask(String question) throws Exception {
-        try {
-            String flaskUrl = "http://localhost:5000/process";
+        ResponseDTO<String> responseDTO = ResponseDTO.<String>builder()
+                .data(List.of(response))
+                .build();
 
-            // 플라스크 서버에 전송할 JSON 데이터
-            String requestData = String.format("{\"question\": \"%s\"}", question);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    public String handleFlask(List<String> selectedItems) {
+        try {
+            String flaskUrl = "http://localhost:5000/survey";
+
+            // 선택 항목을 JSON 형식으로 변환하여 전송
+            ObjectMapper objectMapper = new ObjectMapper();
+            String requestData = objectMapper.writeValueAsString(Map.of("selectedItems", selectedItems));
 
             // HTTP 요청 헤더 설정
             HttpHeaders headers = new HttpHeaders();
@@ -45,11 +50,11 @@ public class SurveyController {
             // HTTP 요청 엔티티 생성
             HttpEntity<String> requestEntity = new HttpEntity<>(requestData, headers);
 
-            // RestTemplate을 사용하여 플라스크 서버에 POST 요청 전송
+            // RestTemplate을 사용하여 Flask 서버에 POST 요청 전송
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<String> responseEntity = restTemplate.postForEntity(flaskUrl, requestEntity, String.class);
 
-            // 플라스크 서버로부터 받은 JSON 응답 데이터
+            // Flask 서버로부터 받은 JSON 응답 데이터
             String responseBody = responseEntity.getBody();
 
             return responseBody;
