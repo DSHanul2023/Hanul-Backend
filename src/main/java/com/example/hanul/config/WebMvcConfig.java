2c6da5f9c9e7a3d83dc96b97698a18b532da433e
chaeeunnn <90565473+chaeeunnn@users.noreply.github.com>
2023-11-03 03:11:07 +0900

    Update WebMvcConfig.java

--- src/main/java/com/example/hanul/config/WebMvcConfig.java
+++ src/main/java/com/example/hanul/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://43.201.147.216/")
+                .allowedOrigins("http://localhost:3000", "http://43.201.147.216")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
